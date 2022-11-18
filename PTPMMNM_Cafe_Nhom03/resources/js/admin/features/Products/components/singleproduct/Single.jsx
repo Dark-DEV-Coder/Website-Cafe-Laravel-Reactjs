98940c0fc53dcc8f49a1e5793a16869736350b88
luanym1234vip <camduyen20012@gmail.com>
2022-11-18 11:24:47 +0700

    Update Single Product v2

--- PTPMMNM_Cafe_Nhom03/resources/js/admin/features/Products/components/singleproduct/Single.jsx
+++ PTPMMNM_Cafe_Nhom03/resources/js/admin/features/Products/components/singleproduct/Single.jsx
@@ -1,22 +1,39 @@
 import Sidebar from '../../../../components/sidebar/Sidebar';
 import Navbar from '../../../../components/navbar/Navbar';
 import "./Single.scss";
+import Select from "react-select";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
 import { listChucNang } from '../../../../../listTest';
-const SingleProduct = ({ inputs, title }) => {
+import { ModalFooter } from 'react-bootstrap';
+const SingleProduct = ({ title }) => {
     const [file, setFile] = useState("");
+    const [input, setInput] = useState(true);
+    const [showButtonEdit, setShowButtonEdit] = useState(false);
+    const handleEditClick = () => {
+        setInput(false);
+        setShowButtonEdit(true);
+    }
     console.log(file);
+    const options = [
+        { value: 'ad', label: 'Admin' },
+        { value: 'nv', label: 'Nhân viên' },
+        { value: 'ql', label: 'Quản lý' }
+    ]
     return (
-        <div className="new">
+        <div className="single">
             <Sidebar chucNangList={listChucNang} />
-            <div className="newContainer">
+            <div className="singleContainer">
                 <Navbar />
-                <div className="top">
+                <div className="singletop">
                     <h1>{title}</h1>
+
                 </div>
-                <div className="bottom">
-                    <div className="left">
+                <div className="singlebottom" style={{ height: "550px" }}>
+
+                    <div className="singleleft">
                         <img
                             src={
                                 file
@@ -26,9 +43,11 @@ const SingleProduct = ({ inputs, title }) => {
                             alt=""
                         />
                     </div>
-                    <div className="right">
+
+                    <div className="singleright">
+
                         <form>
-                            <div className="formInput">
+                            <div className="singleformInput">
                                 <label htmlFor="file">
                                     Image: <DriveFolderUploadOutlinedIcon className="icon" />
                                 </label>
@@ -39,23 +58,56 @@ const SingleProduct = ({ inputs, title }) => {
                                     style={{ display: "none" }}
                                 />
                             </div>
+                            <div className="singleformInput" >
 
-                            {inputs.map((input) => (
-                                <div className="formInput" key={input.id}>
-                                    <label>{input.label}</label>
-                                    <input type={input.type} placeholder={input.placeholder} />
-                                </div>
-                            ))}
-                            <div className="formInput" >
+                            </div>
+                            <div className='singleformInput'>
+                                <label>Mã Sản Phẩm</label>
+                                <input type={"text"} placeholder={"ABC"} disabled={input} />
+                            </div>
+
+                            <div className='singleformInput'>
+                                <label>Tên sản phẩm</label>
+                                <input type={"text"} placeholder={"ABC"} disabled={input} />
+                            </div>
+
+                            <div className="singleformInput" >
+                                <label>Mã Loại Sản Phẩm</label>
+                                <select value={12} className="select-css" disabled={input}>
+                                    <option value='-1'  >Chọn Mã Loại Sản Phẩm</option>
+
+                                </select>
+                            </div>
+                            <div className="singleformInput" >
+                                <label>Mã Nhà Cung Cấp</label>
+                                <select value={12} className="select-css" disabled={input} >
+                                    <option value='-1'  >Chọn Mã Nhà Cung Cấp</option>
+
+                                </select>
+                            </div>
+                            <div className='singleformInput'>
+                                <label>Số Lượng</label>
+                                <input type={"number"} placeholder={"123000"} disabled={input} />
+                            </div>
+                            <div className='singleformInput'>
+                                <label>Giá</label>
+                                <input type={"number"} placeholder={"123000"} disabled={input} />
+                            </div>
+                            <div className='singleformInput'>
+                                <label>Mô Tả</label>
+                                <textarea className="form-input" placeholder="Tell us about yourself" rows={4} disabled={input}></textarea>
+                            </div>
+
+                            <div className="singleformInput"   >
+                                {showButtonEdit ? <button>Lưu</button> : null}
 
                             </div>
 
-                            <div className="formInput" >
-                                <button>Send</button>
-                            </div>
+
 
                         </form>
                     </div>
+                    <div className="singleButtonEdit" onClick={() => handleEditClick()}>Edit</div>
                 </div>
             </div>
         </div>
